4e7244cfa88a39824c7bd38326fe02e12b7ef015
Ahmed Ramy <ahmedramy.e404@gmail.com>
2022-04-16 12:26:56 +0200

    feat: :sparkles: Created routes and schema

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
-// async errors
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect')
-
+const productsRouter = require('./routes/products');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.json());
-app.use(notFoundMiddleware);
-app.use(errorMiddleware);
+
 
 // routes
 
@@ -20,7 +19,12 @@ app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href = "/api/v1/products">products route</a>')
 })
 
+app.use('/api/v1/products', productsRouter);
+
 // products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 3000;
 
